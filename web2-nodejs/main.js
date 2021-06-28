fe9227235045b3778f5ae66a2bbf143ecc617e24
addy.b <addy.b@kakaoenterprise.com>
2021-06-29 08:31:20 +0900

    compression middleware 사용

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -2,11 +2,13 @@ var url = require('url');
 var topic = require('./lib/topic');
 var author = require('./lib/author')
 var bodyParser = require('body-parser')
+var compress = require('compress')
 
 const express = require('express')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(compress())
 app.get('/', function(req, res) {
     var _url = req.url ;
     var queryData = url.parse(_url, true).query;
